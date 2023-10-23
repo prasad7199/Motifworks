75722547210c495d3c6ddb9be221e02b7e0f4c6a
prasad7199 <138664466+prasad7199@users.noreply.github.com>
2023-10-23 17:02:43 +0530

    Add files via upload

---
+++ gitUpload3/Models/PracticeContext.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace api3.Models;
+
+public partial class PracticeContext : DbContext
+{
+    public PracticeContext()
+    {
+    }
+
+    public PracticeContext(DbContextOptions<PracticeContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    public virtual DbSet<Table> Tables { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=(LocalDB)\\MSSQLLocalDB;Database=practice;Trusted_Connection=true;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.HasKey(e => e.EmployeeId).HasName("PK__Employee__C52E0BA874F4617F");
+
+            entity.ToTable("Employee");
+
+            entity.Property(e => e.EmployeeId).HasColumnName("employee_id");
+            entity.Property(e => e.Address)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("address");
+            entity.Property(e => e.Age).HasColumnName("age");
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("name");
+        });
+
+        modelBuilder.Entity<Table>(entity =>
+        {
+            entity.HasKey(e => e.EmployeeId).HasName("PK__Table__C52E0BA8EA04383D");
+
+            entity.ToTable("Table");
+
+            entity.Property(e => e.EmployeeId).HasColumnName("employee_id");
+            entity.Property(e => e.Address)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("address");
+            entity.Property(e => e.Age).HasColumnName("age");
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("name");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ gitUpload3/Models/Table.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace api3.Models;
+
+public partial class Table
+{
+    public int EmployeeId { get; set; }
+
+    public string? Name { get; set; }
+
+    public int? Age { get; set; }
+
+    public string? Address { get; set; }
+}
