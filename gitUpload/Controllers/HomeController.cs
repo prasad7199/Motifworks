f460e393b3a9a1fded1ec4fcf00ec17b0742c846
prasad7199 <138664466+prasad7199@users.noreply.github.com>
2023-10-23 16:32:29 +0530

    Add files via upload

---
+++ gitUpload/Controllers/HomeController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using api2.Models;
+
+namespace api2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly PracticeContext _context;
+
+        public HomeController(PracticeContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Home
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Table>>> GetTables()
+        {
+          if (_context.Tables == null)
+          {
+              return NotFound();
+          }
+            return await _context.Tables.ToListAsync();
+        }
+
+        // GET: api/Home/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Table>> GetTable(int id)
+        {
+          if (_context.Tables == null)
+          {
+              return NotFound();
+          }
+            var table = await _context.Tables.FindAsync(id);
+
+            if (table == null)
+            {
+                return NotFound();
+            }
+
+            return table;
+        }
+
+        // PUT: api/Home/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTable(int id, Table table)
+        {
+            if (id != table.EmployeeId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(table).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TableExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Home
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Table>> PostTable(Table table)
+        {
+          if (_context.Tables == null)
+          {
+              return Problem("Entity set 'PracticeContext.Tables'  is null.");
+          }
+            _context.Tables.Add(table);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTable", new { id = table.EmployeeId }, table);
+        }
+
+        // DELETE: api/Home/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTable(int id)
+        {
+            if (_context.Tables == null)
+            {
+                return NotFound();
+            }
+            var table = await _context.Tables.FindAsync(id);
+            if (table == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tables.Remove(table);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TableExists(int id)
+        {
+            return (_context.Tables?.Any(e => e.EmployeeId == id)).GetValueOrDefault();
+        }
+    }
+}
